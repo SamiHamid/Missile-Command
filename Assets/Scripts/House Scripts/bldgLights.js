cbc657131b4194f4c77d5733bb4dcbad92477a0f
asmccormick <asmccormick@gmail.com>
2015-06-08 15:21:43 -0400

    building lights during destruction
    - lights are only blue
    - lights get slightly dimmer with each collision
    - reduced delay after impact before lights begin changing

--- Assets/Scripts/House Scripts/bldgLights.js
+++ Assets/Scripts/House Scripts/bldgLights.js
@@ -1,9 +1,10 @@
 ﻿#pragma strict
-
+private var delay : float = 0.1;
 private var spawnTime : float;
 
 function Start () {
 	spawnTime = Time.time;
+	//Debug.Log ("r = " + GetComponent.<Renderer>().material.color);
 }
 
 function Update () {
@@ -11,10 +12,14 @@ function Update () {
 }
 
 function OnCollisionEnter(collision) {
-	if (Time.time > spawnTime + 1) {	// slight delay so that blocks don't all change color at spawning
+	if (Time.time > spawnTime + delay) {	// slight delay so that blocks don't all change color at spawning
+		/*
 		var r = Random.Range(0.0,0.5);
 		var g = Random.Range(0.0,0.5);
 		var b = Random.Range(0.0,0.5);
-		GetComponent.<Renderer>().material.color = Color(r,g,b);
+		GetComponent.<Renderer>().material.color = Color(r,g,b) ;
+		*/
+		var currentColor = GetComponent.<Renderer>().material.color;
+		GetComponent.<Renderer>().material.color = currentColor * 0.9;
 	}
 }
