8cbedf86808b0de3bb864ab2e9d4982ccb2143cc
SamiHamid <samihamid@hotmail.co.uk>
2015-06-16 16:17:06 +0300

    Rebuilt UI & adjust grid size
    Rebuilt UI & adjust grid size
    Press mouse button to go through UI, or Space to skip it.

---
+++ Assets/Scripts/UI/StartUpScript.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartUpScript : MonoBehaviour {
+
+	// StartUp Menu
+	private Animator anim;
+	private int StartUpCount = 0;
+	
+	// How To Menu 
+	public GameObject HowToMiddleObj;
+	public GameObject HowToLeftObj;
+	public GameObject HowToRightObj;
+	private HowtoScript MiddleScript;
+	private HowToRight RightScript;
+	private HowToLeft LeftScript;
+	
+	// Start Game
+	public GameObject Grid;
+	public GameObject EnvironmentUI;
+	private gridMakerScript GridScript;
+	private EnvironmentUIScript EnvScript;
+	
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+		MiddleScript = HowToMiddleObj.GetComponent<HowtoScript>();
+		LeftScript = HowToLeftObj.GetComponent<HowToLeft>();
+		RightScript = HowToRightObj.GetComponent<HowToRight>();
+		GridScript = Grid.GetComponent<gridMakerScript>();
+		EnvScript = EnvironmentUI.GetComponent<EnvironmentUIScript>();
+	}
+	
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0))
+		{	
+			// Start In
+			if (StartUpCount == 0)
+			{
+				anim.SetTrigger ("FadeIn");
+				StartUpCount = 1;
+			}
+			// Start Fade & HowTo In
+			else if (StartUpCount == 1)
+			{
+				anim.SetTrigger ("FadeOut");
+				StartUpCount  = 2;
+				MiddleScript.FadeIn();
+				LeftScript.FadeIn();
+				RightScript.FadeIn();
+				StartUpCount = 2;
+			}
+			// HowTo Out
+			else if (StartUpCount == 2)
+			{
+				MiddleScript.FadeOut();
+				LeftScript.FadeOut();
+				RightScript.FadeOut();
+				Invoke ("GameOn", 5);
+			}
+		}
+		//SKIP STARTUP MENU
+		if (Input.GetButtonDown("Jump"))
+		{
+			GameOn();
+			HowToMiddleObj.SetActive(false);
+			HowToLeftObj.SetActive(false);
+			HowToRightObj.SetActive(false);	
+		}
+		
+		if (Input.GetButton("Cancel"))
+		{
+			Application.Quit();
+		}
+	}
+	
+	// Game Begins
+	void GameOn()
+	{
+		if (!gridMakerScript.InitializationStarted && !GameManager.GameStarted)
+		{
+		GridScript.GameBegin();
+		EnvScript.Activate();
+		}
+	}
+}
