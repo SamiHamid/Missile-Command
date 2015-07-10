edd0dca136796aca4051512c09e85914f1d970bc
asmccormick <asmccormick@gmail.com>
2015-07-10 13:25:26 +0300

    flying cars

---
+++ Assets/Scripts/ChickenScript.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+var startPos : Vector3;
+var endPos : Vector3;
+var startTime : float = 0.0;
+var journeyTime : float = 5.0;
+
+function Start () {
+	startPos = Random.insideUnitSphere * 100;
+	startPos.y = 1;
+	//Debug.Log("chick startPos = " + startPos);
+	endPos = Random.insideUnitSphere * 100;
+	endPos.y = 1;
+}
+
+function Update () {
+ 	var fracComplete = (Time.time - startTime) / journeyTime;
+	transform.position = Vector3.Slerp(startPos, endPos, fracComplete);
+	if (Vector3.Distance(transform.position, endPos) < 1){
+		startPos = endPos;
+		endPos = Random.insideUnitSphere * 100;
+		endPos.y = 1;
+		startTime = Time.time;
+	}
+}
---
+++ Assets/Scripts/FlyingCarScript.js
@@ -0,0 +1,57 @@
+﻿#pragma strict
+var startPos : Vector3;
+var endPos : Vector3;
+private var rb : Rigidbody;
+var exhaust : GameObject;
+var force : float = 15;
+
+function Start () {
+	startPos.x = Random.Range(-75.0, 75);
+	startPos.y = 10;
+	startPos.z = Random.Range(-30, 115);
+	transform.position = startPos;
+	endPos.x = startPos.x + Random.Range(-10, 10);
+	endPos.y = Random.Range(10, 25);
+	endPos.z = startPos.z + Random.Range(-10, 10);
+	//endPos.y = 1000;
+	rb = GetComponent.<Rigidbody>();
+	exhaust.SetActive (false);
+}
+
+function Update () {
+	if (Vector3.Distance(transform.position, endPos) > 5) {
+		
+		// move car Y-axis
+		if (transform.position.y < endPos.y) {
+			rb.AddRelativeForce(transform.up * force);
+			if (exhaust.activeSelf == false) {
+				exhaust.SetActive (true);
+			}
+		} else {
+			if (exhaust.activeSelf == true) {
+				exhaust.SetActive (false);
+				rb.AddRelativeForce(transform.up * force * 0.5);
+			}
+		}
+		
+		// move car X-axis
+		if (transform.position.x > endPos.x) {
+			rb.AddForce(transform.right * -force * 0.1);
+		} else {
+			rb.AddForce(transform.right * force * 0.1);
+		}
+		
+		// move car Z-axis
+		if (transform.position.z > endPos.z) {
+			rb.AddForce(transform.forward * -force * 0.1);
+		} else {
+			rb.AddForce(transform.forward * force * 0.1);
+		}
+	} else {
+		Debug.Log("flying car finds new endPos");
+		endPos.x = Random.Range(-75.0, 75);
+		endPos.y = Random.Range(5, 25);
+		endPos.z = Random.Range(-30,115);
+	}
+	
+}
