950787413bd1648d5b6b6c6a74b4627465211cdd
asmccormick <asmccormick@gmail.com>
2015-07-12 14:01:04 +0300

    cleanup Mothership script

--- Assets/Scripts/MothershipScript.js
+++ Assets/Scripts/MothershipScript.js
@@ -2,7 +2,6 @@
 var startPos : Vector3;
 var endPos : Vector3;
 private var rb : Rigidbody;
-//var exhaust : GameObject;
 var exhaust : ParticleSystem;
 var force : float = 15;
 var impactPoint : GameObject;
@@ -15,16 +14,14 @@ function Start () {
 	endPos.x = startPos.x + Random.Range(-10, 10);
 	endPos.y = Random.Range(10, 25);
 	endPos.z = Random.Range(0, 100);
-	//endPos.y = 1000;
 	rb = GetComponent.<Rigidbody>();
-	//exhaust.SetActive (false);
 	exhaust.Stop();
 }
 
 function Update () {
 	if (Vector3.Distance(transform.position, endPos) > 5) {
 		
-		// move car Y-axis
+		// move on Y-axis
 		if (transform.position.y < endPos.y) {
 			rb.AddForce(transform.up * force);
 			if (exhaust.isPlaying == false) {
@@ -37,21 +34,21 @@ function Update () {
 			}
 		}
 		
-		// move car X-axis
+		// move on X-axis
 		if (transform.position.x > endPos.x) {
 			rb.AddForce(transform.right * -force);  // was *0.1
 		} else {
 			rb.AddForce(transform.right * force);
 		}
 		
-		// move car Z-axis
+		// move on Z-axis
 		if (transform.position.z > endPos.z) {
 			rb.AddForce(transform.forward * -force);
 		} else {
 			rb.AddForce(transform.forward * force);
 		}
 	} else {
-		Debug.Log("flying car finds new endPos");
+		// find a new destination
 		endPos.x = Random.Range(-75.0, 75);
 		endPos.y = Random.Range(10, 25);
 		endPos.z = Random.Range(0,100);
